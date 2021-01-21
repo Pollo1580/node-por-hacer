3f379100459fdd414f044d3b1c856130ed1c96aa
Emmanuel Cortes <hbc_gap@hotmail.com>
2021-01-21 17:39:00 -0600

    Primer commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+## Aplicacion de comandos
+
+Recuerden instalar los paquetes de Node.js
+
+````
+
+npm install
+
+````
---
+++ config/yargs.js
@@ -0,0 +1,39 @@
+//requireds
+const descripcion = {
+
+    demand: true,
+    alias: 'd',
+    desc: 'Descripcion de la tarea por hacer'
+
+
+};
+
+const completado = {
+
+    alias: 'c',
+    default: true,
+    desc: 'Marcar como completado o pendiente la tarea'
+
+};
+
+
+
+const argv = require('yargs')
+    .command('crear', 'Crea un elemento por hacer', {
+        descripcion
+    })
+
+.command('actualizar', 'Actualiza el estado completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borrando la tarea', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+
+module.exports = {
+    argv
+}
